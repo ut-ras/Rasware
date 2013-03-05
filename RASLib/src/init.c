e68ef5849747f7ffa21f6053a76174f39457eeeb
Frank Weng <weng.frank@utexas.edu>
2013-03-05 03:47:05 -0600

    Cleaning, minor change to linking, correction to comment header

--- RASLib/src/init.c
+++ RASLib/src/init.c
@@ -5,31 +5,38 @@
 // THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
 // NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
 // NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHORS OF THIS FILE
+// SHALL NOT, UNDER ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+// OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
 // 
 // This is part of RASLib Rev0 of the RASWare2013 package.
 //
+// Written by: 
+// The student branch of the 
+// IEEE - Robotics and Automation Society 
+// at the University of Texas at Austin
+//
 // Website: ras.ece.utexas.edu
-// Contact: president@ras.ece.utexas.edu
+// Contact: rasware@ras.ece.utexas.edu
 //
 //*****************************************************************************
 
 #include "inc/hw_types.h"
-#include "driverlib/interrupt.h"
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "init.h"
 
-void InitializeMCU(void){
+void InitializePeriodicFunctions(void){
 		//
     // Enable lazy stacking for interrupt handlers.  This allows floating-point
     // instructions to be used within interrupt handlers, but at the expense of
     // extra stack usage.
     //
     ROM_FPULazyStackingEnable();
+	  // Set the clocking to run from PLL, using external oscillator
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
                        SYSCTL_XTAL_16MHZ);
-	  IntMasterEnable();
+	  //Enable global interrupts
+	  ROM_IntMasterEnable();
 }
+
