e111fef5199e9c5336a0e867515bf4cd3529d9c5
Frank Weng <weng.frank@utexas.edu>
2013-07-13 12:50:44 -0500

    Seemingly working motor drivers

--- RASLibTest/test.c
+++ RASLibTest/test.c
@@ -4,16 +4,46 @@
 #include "adc.h"
 #include "uart.h"
 #include "servo.h"
+#include "encoder.h"
+#include "motor.h"
+AddServo(One,B,6)
+AddServo(Two,B,7)
 
-AddServo(One,A,2)
+int EncoderMain(void){
+    InitializeMCU();
+    InitializeUART();
+    InitializeEncoder(F,2,3);
+    UARTprintf("Encoder Test Starting...\n");
+    for(;;){
+        UARTprintf("enc0: %d     \r", GetEncoderTicks(0));
+    }
+}
+
+AddMotor(Left,A,4,C,6,COAST)
+int MotorMain(void){
+    InitializeMCU();
+    InitializeUART();
+    InitializeMotorLeft();
+    UARTprintf("\nMotor Test Starting...\n");
+    for(;;){
+        static float i = -1.0;
+        UARTprintf("Motor Output: %g %d   \r",i,(long)(i*10000));
+        SetMotorLeft(i);
+        i += (1)/((float)MOTOR_GENERATOR_RESOLUTION);
+        if(i>1) i=-1;
+        WaitMS(1);
+    }
+}
 int ServoMain(void){
     InitializeMCU();
     InitializeUART();
     InitializeServoOne();
+    InitializeServoTwo();
     UARTprintf("Servo Test Starting...\n");
     for(;;){
         static int i = 0;
         SetServoOne(i);
+        SetServoTwo((i*2)%256);
         UARTprintf("Servo Output: %d   \r",i);
         i = (i+1)%256;
         WaitMS(100);
@@ -24,7 +54,7 @@ int ADCMain(void){
     InitializeMCU();
     InitializeUART();
     InitializeADC();
-    UARTprintf("ADC Test Starting...\n");
+    UARTprintf("\nADC Test Starting...\n");
     for(;;){
         UARTprintf("ADC0: %d ADC1: %d     \r", GetADC(0), GetADC(1));
     }
@@ -34,7 +64,7 @@ int ADCMain(void){
 int BusyWaitMain(void){
     InitializeMCU();
     InitializeUART();
-    UARTprintf("Busy Wait Test Starting...\n");
+    UARTprintf("\nBusy Wait Test Starting...\n");
     for(;;){
         static int count = 0;
         UARTprintf("1/10 Seconds past: %d     \r", count++);
@@ -44,7 +74,7 @@ int BusyWaitMain(void){
 int SysTimeMain(void){
     InitializeMCU();
     InitializeUART();
-    UARTprintf("System Time Test Starting...\n");
+    UARTprintf("\nSystem Time Test Starting...\n");
     for(;;){
         UARTprintf("S: %d mS: %d      \r", g_ulSystemTimeSeconds, g_ulSystemTimeMS);
     }
@@ -58,12 +88,12 @@ void perFuncTest(void){
 int PerFuncMain(void){
     InitializeMCU();
     InitializeUART();
-    UARTprintf("Periodic Functions Test Starting...\n");
+    UARTprintf("\nPeriodic Functions Test Starting...\n");
     AddPeriodicFunction(perFuncTest,1000);
     for(;;);
 }
 
 
 int main(void){
-    return PerFuncMain();
+    return MotorMain();
 }
