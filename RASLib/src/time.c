88616ae6f9d9905a99f623d705288da9ba8f9761
Christopher Haster <chaster@utexas.edu>
2013-07-15 18:53:53 -0600

    Fixed Tabs in time.c
    Really, when would you ever need tabs TWO SPACES LONG

--- RASLib/src/time.c
+++ RASLib/src/time.c
@@ -40,10 +40,10 @@ static long USTicks = 0;
 
 // Relevant task info
 struct task {
-		time_t target;
-	  time_t repeatTime;
-		void *data;
-		void (*callback)(void*);
+    time_t target;
+    time_t repeatTime;
+    void *data;
+    void (*callback)(void*);
 };
 
 // Cyclic buffer of waiting tasks
@@ -59,177 +59,177 @@ struct task taskBuffer[TASK_BUFF_SIZE];
 // Initializes a system timer with microsecond resolution
 void InitializeSystemTime(void) {
     // Reset global clock
-		systemTimeUS = 0;
-	
-		// Reset buffer just in case its been used
-		taskStart = 0;
-		taskEnd = 0;
-	
-		// Find the system clock divided by 1000000, which results in 
-		// 1 US for the timer
-		USTicks = SysCtlClockGet() / US_IN_S;
-	
-		// Enable the timer 
-		SysCtlPeripheralEnable(SYSCTL_PERIPH_WTIMER5);
-	
-		// We use the first half of the timer to keep track of the 
-		// microseconds that have passed
-		// The second half we use as a one shot to trigger pending tasks
+    systemTimeUS = 0;
+  
+    // Reset buffer just in case its been used
+    taskStart = 0;
+    taskEnd = 0;
+  
+    // Find the system clock divided by 1000000, which results in 
+    // 1 US for the timer
+    USTicks = SysCtlClockGet() / US_IN_S;
+  
+    // Enable the timer 
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_WTIMER5);
+  
+    // We use the first half of the timer to keep track of the 
+    // microseconds that have passed
+    // The second half we use as a one shot to trigger pending tasks
     TimerConfigure(WTIMER5_BASE, TIMER_CFG_SPLIT_PAIR | 
                                  TIMER_CFG_A_PERIODIC_UP | 
-                                 TIMER_CFG_B_ONE_SHOT);		
-	
-		// Setup timer A to run for a full second
-		TimerLoadSet(WTIMER5_BASE, TIMER_A, US_IN_S * USTicks);
-		TimerIntEnable(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
-	
-		// Only enable the timer B interrupt when it is needed
-		TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
-		
+                                 TIMER_CFG_B_ONE_SHOT);    
+  
+    // Setup timer A to run for a full second
+    TimerLoadSet(WTIMER5_BASE, TIMER_A, US_IN_S * USTicks);
+    TimerIntEnable(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
+  
+    // Only enable the timer B interrupt when it is needed
+    TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
+    
     // Enable the timers and interrupts
-		IntEnable(INT_WTIMER5A);
-		IntEnable(INT_WTIMER5B);
-		TimerEnable(WTIMER5_BASE, TIMER_BOTH);
+    IntEnable(INT_WTIMER5A);
+    IntEnable(INT_WTIMER5B);
+    TimerEnable(WTIMER5_BASE, TIMER_BOTH);
 }
 
 // Outputs system time in microseconds
 time_t GetTimeUS(void) {
-		return systemTimeUS + 
-					 (TimerValueGet(WTIMER5_BASE, TIMER_A) / USTicks);
+    return systemTimeUS + 
+           (TimerValueGet(WTIMER5_BASE, TIMER_A) / USTicks);
 }
 
 time_t GetTimeS(void) {
-		return GetTimeUS() / US_IN_S;
+    return GetTimeUS() / US_IN_S;
 }
 
 // Handler that simply updates the time by one second
 void WTimer5AHandler(void) {
-		TimerIntClear(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
-		systemTimeUS += US_IN_S;
+    TimerIntClear(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
+    systemTimeUS += US_IN_S;
 }
 
 // Called internally to register a task
 static void RegisterTask(struct task *task) {
-		unsigned int i, prev;
-	
-		// Disable any incoming tasks temporarily
-		TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
-	
-		// Start at the back and work forward moving any later tasks back	
-		for (i = taskEnd; i != taskStart; i = prev) {
-				prev = TASK_MASK(i-1);
-			
-				if (taskBuffer[prev].target > task->target) {
-						taskBuffer[i] = taskBuffer[prev];
-				} else {
-						break;
-				}
-		}
-		
-		// Put our task into the task buffer
-		taskBuffer[i] = *task;
-		
-		// Increase the buffer size
-		taskEnd = TASK_MASK(taskEnd+1);
+    unsigned int i, prev;
+  
+    // Disable any incoming tasks temporarily
+    TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
+  
+    // Start at the back and work forward moving any later tasks back  
+    for (i = taskEnd; i != taskStart; i = prev) {
+        prev = TASK_MASK(i-1);
+      
+        if (taskBuffer[prev].target > task->target) {
+            taskBuffer[i] = taskBuffer[prev];
+        } else {
+            break;
+        }
+    }
+    
+    // Put our task into the task buffer
+    taskBuffer[i] = *task;
+    
+    // Increase the buffer size
+    taskEnd = TASK_MASK(taskEnd+1);
 }
 
 // Setup timer B to trigger an interrupt for the next task
 static void SetNextTaskInt(void) {
-		time_t usUntil;
-	
-		// Check to make sure there even is a task
-		if (taskEnd == taskStart) {
-				TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
-				return;
-		}
+    time_t usUntil;
+  
+    // Check to make sure there even is a task
+    if (taskEnd == taskStart) {
+        TimerIntDisable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
+        return;
+    }
 
-		// Calculate the next task's eta
-		usUntil = taskBuffer[taskStart].target - GetTimeUS();
-			
-		// Check for 32bit overflow in which case we can just 
-		// interrupt as late as possible. The handler will do nothing.
-		TimerLoadSet(WTIMER5_BASE, TIMER_B, 
-								 (usUntil > 0xffffffff) ? 0xffffffff : usUntil);
-			
-		// Enable the actual interrupt
-		TimerIntEnable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
+    // Calculate the next task's eta
+    usUntil = taskBuffer[taskStart].target - GetTimeUS();
+      
+    // Check for 32bit overflow in which case we can just 
+    // interrupt as late as possible. The handler will do nothing.
+    TimerLoadSet(WTIMER5_BASE, TIMER_B, 
+                 (usUntil > 0xffffffff) ? 0xffffffff : usUntil);
+      
+    // Enable the actual interrupt
+    TimerIntEnable(WTIMER5_BASE, TIMER_TIMB_TIMEOUT);
 }
 
 // Handler used to manage waiting tasks
 void WTimer5BHandler(void){
-		TimerIntClear(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
-	
-		// If there is a task waiting, call it and 
-		// remove it from the buffer
-		while (taskStart != taskEnd && 
-					 systemTimeUS >= taskBuffer[taskStart].target) {
-				struct task *task = &taskBuffer[taskStart];
-			
-				task->callback(task->data);
-				
-				// Reregister the function if it wants to repeat
-				if (task->repeatTime) {
-						task->target += task->repeatTime;
-						RegisterTask(task);
-				}
-				
-				// Pop the buffer
-				taskStart = TASK_MASK(taskStart+1);
-		}
-		
-		// Setup the next task
-		SetNextTaskInt();
+    TimerIntClear(WTIMER5_BASE, TIMER_TIMA_TIMEOUT);
+  
+    // If there is a task waiting, call it and 
+    // remove it from the buffer
+    while (taskStart != taskEnd && 
+           systemTimeUS >= taskBuffer[taskStart].target) {
+        struct task *task = &taskBuffer[taskStart];
+      
+        task->callback(task->data);
+        
+        // Reregister the function if it wants to repeat
+        if (task->repeatTime) {
+            task->target += task->repeatTime;
+            RegisterTask(task);
+        }
+        
+        // Pop the buffer
+        taskStart = TASK_MASK(taskStart+1);
+    }
+    
+    // Setup the next task
+    SetNextTaskInt();
 }
 
 // Schedules a callback function to be called in given microseconds
 void CallInUS(void (*callback)(void*), void *data, time_t us) {
-		struct task task;
-	
-		task.target = systemTimeUS + us;
-		// Zero repeatTime indicates it should run once
-		task.repeatTime = 0;
-		task.callback = callback;
-		task.data = data;
-			
-		// Register it to be called
-		RegisterTask(&task);
-		SetNextTaskInt();
+    struct task task;
+  
+    task.target = systemTimeUS + us;
+    // Zero repeatTime indicates it should run once
+    task.repeatTime = 0;
+    task.callback = callback;
+    task.data = data;
+      
+    // Register it to be called
+    RegisterTask(&task);
+    SetNextTaskInt();
 }
 
 void CallInS(void (*callback)(void*), void *data, time_t s) {
-		CallInUS(callback, data, s*US_IN_S);
+    CallInUS(callback, data, s*US_IN_S);
 }
 
 // Schedules a callback function to be called repeatedly
 void CallEveryUS(void (*callback)(void*), void *data, time_t us) {
-		struct task task;
-	
-		task.target = systemTimeUS + us;
-		task.repeatTime = us;
-		task.callback = callback;
-		task.data = data;
-			
-		// Register it to be called
-		RegisterTask(&task);
-		SetNextTaskInt();
+    struct task task;
+  
+    task.target = systemTimeUS + us;
+    task.repeatTime = us;
+    task.callback = callback;
+    task.data = data;
+      
+    // Register it to be called
+    RegisterTask(&task);
+    SetNextTaskInt();
 }
 
 void CallEveryS(void (*callback)(void*), void *data, time_t s) {
-		CallEveryUS(callback, data, s*US_IN_S);
+    CallEveryUS(callback, data, s*US_IN_S);
 }
 
 // Busy waits for given milliseconds
 static void WaitHandler(void *flag) {
-		*(int*)flag = 1;
+    *(int*)flag = 1;
 }
 
 void WaitUS(time_t us) {
-		int waitFlag = 0;
-		CallInUS(WaitHandler, &waitFlag, us);
-		while(!waitFlag);
+    int waitFlag = 0;
+    CallInUS(WaitHandler, &waitFlag, us);
+    while(!waitFlag);
 }
 
 void WaitS(time_t s) {
-		WaitUS(s*US_IN_S);
+    WaitUS(s*US_IN_S);
 }
-		
+    
