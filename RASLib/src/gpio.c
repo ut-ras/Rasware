3652ec62fa8bb704aa7da70d2627bb0c1630f828
Christopher Haster <chaster@utexas.edu>
2013-07-19 20:14:40 -0600

    Tested and applied encoders.
    Encoders have shown to be working indicating that gpio is functional.

--- RASLib/src/gpio.c
+++ RASLib/src/gpio.c
@@ -23,9 +23,12 @@
 //*****************************************************************************
 
 #include "gpio.h"
+#include "inc/hw_ints.h"
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
 #include "internal.h"
 
 // Port table is used externally to lookup port values
@@ -77,6 +80,23 @@ void InitializeGPIO(void) {
     // Just clear all handlers
     for (i=0; i<PIN_COUNT; i++)
         pinTaskBuffer[i].callback = Dummy;
+    
+    // Enable the peripherals
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+        
+    // Enable the interrupts
+    // This shouldn't be nescessary but for some reason is...
+    IntEnable(INT_GPIOA);
+    IntEnable(INT_GPIOB);
+    IntEnable(INT_GPIOC);
+    IntEnable(INT_GPIOD);
+    IntEnable(INT_GPIOE);
+    IntEnable(INT_GPIOF);
 }
 
 // Get Pin value as a boolean
@@ -102,20 +122,27 @@ void SetPinZ(tPin pin) {
     // Setting pin direction to input places it in high impedance mode
     GPIOPinTypeGPIOInput(PORT_VAL(pin), PIN_VAL(pin));
 }
-    
 
 // Register a callback to be called when the pin's value changes, 
 // the state of the pin can then be determined through the GetPin function.
 void CallOnPin(tCallback callback, void *data, tPin pin) {
     tPinTask *task = &pinTaskBuffer[pin];
-    
-    // Put in the dummy first to avoid a race condition
+
+    // Stop the interrupt first to avoid a race condition
+    GPIOPinIntDisable(PORT_VAL(pin), PIN_VAL(pin));
     task->callback = Dummy;
+    
+    // Make sure the pin is setup as an input
+    GPIOPinTypeGPIOInput(PORT_VAL(pin), PIN_VAL(pin));
     
     // If a null pointer is passed then we just leave the Dummy
     // to unregister the callback
     if (callback) {
         task->data = data;
         task->callback = callback;
+        
+        // Setup the interrupts
+        GPIOPinIntEnable(PORT_VAL(pin), PIN_VAL(pin));
+        GPIOIntTypeSet(PORT_VAL(pin), PIN_VAL(pin), GPIO_BOTH_EDGES);
     }
 }
