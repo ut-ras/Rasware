8374321db092dd9ddd71650d66e8b5a790226fa7
Christopher Haster <chaster@utexas.edu>
2013-07-30 03:45:35 -0500

    Redesigned the ADC module to reflect design changes in RASWare
    Incorporated various features such as pin definitions and timing functions
    and redesigned the overall module to better match the standards that have
    been created for RASWare.
    
    Also adds background and busy wait functions that are more cpu expensive
    than the contious reads but allow the option. The ADC module also assigns
    the created ADC pins to the available ADC blocks in a staggered way to
    balance to load and hopefully optimize performance.
    
    Has not yet been tested for functionality.

--- RASLib/inc/adc.h
+++ RASLib/inc/adc.h
@@ -21,9 +21,52 @@
 //
 //*****************************************************************************
 
-#define ADC_SAMPLE_RATE 2000
-#define HW_OVERSAMPLING_FACTOR 64
+#include "time.h"
+#include "gpio.h"
 
-typedef enum{ADC0,ADC1,ADC2,ADC3,ADC4,ADC5,ADC6,ADC7,ADC8,ADC9,ADC10,ADC11,ADC_BUFFER_SIZE} tADC;
-extern unsigned long g_rgADCValues[ADC_BUFFER_SIZE];
-unsigned long GetADC(tADC adc);
+// Constant for ADC oversampling
+// Can be 0, 2, 4, 8, 16, 32, 64
+// Doubling the oversample doubles to time to read ADC
+#define ADC_OVERSAMPLING_FACTOR 32
+
+// Definition of struct ADC in adc.c
+typedef struct ADC tADC;
+
+// ADC is only supported on a limited number of pins in hardware
+// The following pins are supported:
+// PIN_B4
+// PIN_B5
+// PIN_D0
+// PIN_D1
+// PIN_D2
+// PIN_D3
+// PIN_E0
+// PIN_E1
+// PIN_E2
+// PIN_E3
+// PIN_E4
+// PIN_E5
+#define ADC_COUNT 12
+
+// Function to initialize an ADC on a pin, if ADC
+// is not supported in hardware on the given pin, 
+// then a null pointer is returned
+// The returned pointer can be used by the ADCRead functions
+tADC *InitializeADC(tPin pin);
+
+// This function returns the value measured as a percentage
+// If the ADC is not continously reading,
+// then the function will busy wait for the results
+float ADCRead(tADC *adc);
+
+// This function sets up an ADC to be run in the background
+// A callback can be passed, in which a call to ADCRead 
+// will return with the newly obtained value immediately
+void ADCBackgroundRead(tADC *snr, tCallback callback, void *data);
+
+// These function set up an ADC to read indefinitly
+// Any following calls to ADCRead will return the most recent value
+// If the passed time between calls is less than the time it takes for
+// the ADC to complete, the ADC will read as fast as possible without overlap
+void ADCReadContinouslyUS(tADC *snr, tTime us);
+void ADCReadContinously(tADC *snr, float s);
