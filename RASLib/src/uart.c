617aa10b033d84c5e44c621368cc21ae4bb0f127
Josh Eversmann <josh.eversmann@utexas.edu>
2013-08-16 23:31:05 -0500

    Moved convert to a function and made X different from x

--- RASLib/src/uart.c
+++ RASLib/src/uart.c
@@ -32,9 +32,12 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
 #include "driverlib/debug.h"
+#include "driverlib/uart.c"
+#include "utils/uartstdio.c"
 #include "uart.h"
 
-static const char * const g_pcHex = "0123456789abcdef";
+static const char * const g_pcHex_U = "0123456789ABCDEF";
+static const char * const g_pcHex_L = "0123456789abcdef";
 
 // Sets up a simple console through UART0
 void InitializeUART(void)
@@ -79,77 +82,120 @@ unsigned char getc(void)
 	return UARTgetc();
 }
 
+int keyWasPressed(void) {
+	if (UARTCharsAvail(UART0_BASE))	
+		return 1;
+	else
+		return 0;
+}
+
+void convert(unsigned long ulValue, unsigned long ulCount, const char *pcHex, char ulNeg, char cFill, unsigned long ulBase)
+{
+	char pcBuf[16];
+	unsigned long ulIdx, ulPos = 0;
+	
+	for(ulIdx = 1;
+			(((ulIdx * ulBase) <= ulValue) &&
+			(((ulIdx * ulBase) / ulBase) == ulIdx));
+      ulIdx *= ulBase, ulCount--)
+  {
+  }
+	
+	// If the value is negative, reduce the count of padding
+	// characters needed.
+	if(ulNeg)
+	{
+			ulCount--;
+	}
+
+	// If the value is negative and the value is padded with
+	// zeros, then place the minus sign before the padding.
+	if(ulNeg && (cFill == '0'))
+	{
+			// Place the minus sign in the output buffer.
+			pcBuf[ulPos++] = '-';
+
+			// The minus sign has been placed, so turn off the
+			// negative flag.
+			ulNeg = 0;
+	}
+
+	// Provide additional padding at the beginning of the
+	// string conversion if needed.
+	if((ulCount > 1) && (ulCount < 16))
+	{
+			for(ulCount--; ulCount; ulCount--)
+			{
+					pcBuf[ulPos++] = cFill;
+			}
+	}
+
+	// If the value is negative, then place the minus sign
+	// before the number.
+	if(ulNeg)
+	{
+			// Place the minus sign in the output buffer.
+			pcBuf[ulPos++] = '-';
+	}
+
+	// Convert the value into a string.
+	for(; ulIdx; ulIdx /= ulBase)
+	{
+			pcBuf[ulPos++] = pcHex[(ulValue / ulIdx) % ulBase];
+	}
+
+	// Write the string.
+	UARTwrite(pcBuf, ulPos);
+}
+
 void printf(const char *pcString, ...)
 {
-	unsigned long ulIdx, ulValue, ulPos, ulCount, ulBase, ulNeg;
-  char *pcStr, pcBuf[16], cFill;
+	unsigned long ulValue, ulIdx, ulCount;
+  char *pcStr, ulNeg, cFill;
+	const char *pcHex;
   va_list vaArgP;
 
-    //
     // Check the arguments.
-    //
     ASSERT(pcString != 0);
 
-    //
     // Start the varargs processing.
-    //
     va_start(vaArgP, pcString);
 
-    //
     // Loop while there are more characters in the string.
-    //
     while(*pcString)
     {
-        //
         // Find the first non-% character, or the end of the string.
-        //
         for(ulIdx = 0; (pcString[ulIdx] != '%') && (pcString[ulIdx] != '\0');
             ulIdx++)
         {
         }
 
-        //
         // Write this portion of the string.
-        //
         UARTwrite(pcString, ulIdx);
 
-        //
         // Skip the portion of the string that was written.
-        //
         pcString += ulIdx;
 
-        //
         // See if the next character is a %.
-        //
         if(*pcString == '%')
         {
-            //
             // Skip the %.
-            //
             pcString++;
 
-            //
             // Set the digit count to zero, and the fill character to space
             // (i.e. to the defaults).
-            //
-            ulCount = 0;
+						ulCount = 0;
             cFill = ' ';
-
-            //
-            // It may be necessary to get back here to process more characters.
-            // Goto's aren't pretty, but effective.  I feel extremely dirty for
-            // using not one but two of the beasts.
-            //
+					
+						// Presets the template string to lowercase
+						pcHex = g_pcHex_L;
+					
 again:
 
-            //
             // Determine how to handle the next character.
-            //
             switch(*pcString++)
             {
-                //
                 // Handle the digit characters.
-                //
                 case '0':
                 case '1':
                 case '2':
@@ -161,125 +207,105 @@ again:
                 case '8':
                 case '9':
                 {
-                    //
                     // If this is a zero, and it is the first digit, then the
                     // fill character is a zero instead of a space.
-                    //
                     if((pcString[-1] == '0') && (ulCount == 0))
                     {
                         cFill = '0';
                     }
 
-                    //
                     // Update the digit count.
-                    //
                     ulCount *= 10;
                     ulCount += pcString[-1] - '0';
 
-                    //
                     // Get the next character.
-                    //
                     goto again;
                 }
 
-                //
                 // Handle the %c command.
-                //
                 case 'c':
                 {
-                    //
                     // Get the value from the varargs.
-                    //
                     ulValue = va_arg(vaArgP, unsigned long);
 
-                    //
                     // Print out the character.
-                    //
                     UARTwrite((char *)&ulValue, 1);
 
-                    //
                     // This command has been handled.
-                    //
                     break;
                 }
 
-                //
                 // Handle the %d and %i commands.
-                //
                 case 'd':
                 case 'i':
                 {
-                    //
                     // Get the value from the varargs.
-                    //
                     ulValue = va_arg(vaArgP, unsigned long);
-
-                    //
-                    // Reset the buffer position.
-                    //
-                    ulPos = 0;
-
-                    //
+									
                     // If the value is negative, make it positive and indicate
                     // that a minus sign is needed.
-                    //
                     if((long)ulValue < 0)
                     {
-                        //
                         // Make the value positive.
-                        //
                         ulValue = -(long)ulValue;
 
-                        //
                         // Indicate that the value is negative.
-                        //
                         ulNeg = 1;
                     }
                     else
                     {
-                        //
                         // Indicate that the value is positive so that a minus
                         // sign isn't inserted.
-                        //
                         ulNeg = 0;
                     }
 
-                    //
-                    // Set the base to 10.
-                    //
-                    ulBase = 10;
-
-                    //
                     // Convert the value to ASCII.
-                    //
-                    goto convert;
+                    convert(ulValue, ulCount, pcHex, ulNeg, cFill, 10);
+										break;
                 }
+								
+								// Handle the %o command.
+								case 'o':
+								{
+                    // Get the value from the varargs.
+                    ulValue = va_arg(vaArgP, unsigned long);
 
-                //
+                    // If the value is negative, make it positive and indicate
+                    // that a minus sign is needed.
+                    if((long)ulValue < 0)
+                    {
+                        // Make the value positive.
+                        ulValue = -(long)ulValue;
+
+                        // Indicate that the value is negative.
+                        ulNeg = 1;
+                    }
+                    else
+                    {
+                        // Indicate that the value is positive so that a minus
+                        // sign isn't inserted.
+                        ulNeg = 0;
+                    }
+
+                    // Convert the value to ASCII.
+                    convert(ulValue, ulCount, pcHex, ulNeg, cFill, 8);
+										break;
+								}
                 // Handle the %s command.
-                //
                 case 's':
                 {
-                    //
                     // Get the string pointer from the varargs.
-                    //
                     pcStr = va_arg(vaArgP, char *);
 
-                    //
                     // Determine the length of the string.
-                    //
                     for(ulIdx = 0; pcStr[ulIdx] != '\0'; ulIdx++)
                     {
                     }
 
-                    //
                     // Write the string.
-                    //
                     UARTwrite(pcStr, ulIdx);
 
-                    //
                     // Write any required padding spaces
-                    //
                     if(ulCount > ulIdx)
                     {
                         ulCount -= ulIdx;
@@ -288,186 +314,61 @@ again:
                             UARTwrite(" ", 1);
                         }
                     }
-                    //
                     // This command has been handled.
-                    //
                     break;
                 }
 
-                //
                 // Handle the %u command.
-                //
                 case 'u':
                 {
-                    //
                     // Get the value from the varargs.
-                    //
                     ulValue = va_arg(vaArgP, unsigned long);
 
-                    //
-                    // Reset the buffer position.
-                    //
-                    ulPos = 0;
-
-                    //
-                    // Set the base to 10.
-                    //
-                    ulBase = 10;
-
-                    //
                     // Indicate that the value is positive so that a minus sign
                     // isn't inserted.
-                    //
                     ulNeg = 0;
 
-                    //
                     // Convert the value to ASCII.
-                    //
-                    goto convert;
+                    convert(ulValue, ulCount, pcHex, ulNeg, cFill, 10);
+										break;
                 }
 
-                //
-                // Handle the %x and %X commands.  Note that they are treated
-                // identically; i.e. %X will use lower case letters for a-f
-                // instead of the upper case letters is should use.  We also
-                // alias %p to %x.
-                //
-                case 'x':
+                // Handle the %x and %X commands.  We alias %p to %x.
                 case 'X':
+									//Make the template string uppercase
+									pcHex = g_pcHex_U;
+                case 'x':
                 case 'p':
                 {
-                    //
                     // Get the value from the varargs.
-                    //
                     ulValue = va_arg(vaArgP, unsigned long);
 
-                    //
-                    // Reset the buffer position.
-                    //
-                    ulPos = 0;
-
-                    //
-                    // Set the base to 16.
-                    //
-                    ulBase = 16;
-
-                    //
                     // Indicate that the value is positive so that a minus sign
                     // isn't inserted.
-                    //
                     ulNeg = 0;
-
-                    //
-                    // Determine the number of digits in the string version of
-                    // the value.
-                    //
-convert:
-                    for(ulIdx = 1;
-                        (((ulIdx * ulBase) <= ulValue) &&
-                         (((ulIdx * ulBase) / ulBase) == ulIdx));
-                        ulIdx *= ulBase, ulCount--)
-                    {
-                    }
-
-                    //
-                    // If the value is negative, reduce the count of padding
-                    // characters needed.
-                    //
-                    if(ulNeg)
-                    {
-                        ulCount--;
-                    }
-
-                    //
-                    // If the value is negative and the value is padded with
-                    // zeros, then place the minus sign before the padding.
-                    //
-                    if(ulNeg && (cFill == '0'))
-                    {
-                        //
-                        // Place the minus sign in the output buffer.
-                        //
-                        pcBuf[ulPos++] = '-';
-
-                        //
-                        // The minus sign has been placed, so turn off the
-                        // negative flag.
-                        //
-                        ulNeg = 0;
-                    }
-
-                    //
-                    // Provide additional padding at the beginning of the
-                    // string conversion if needed.
-                    //
-                    if((ulCount > 1) && (ulCount < 16))
-                    {
-                        for(ulCount--; ulCount; ulCount--)
-                        {
-                            pcBuf[ulPos++] = cFill;
-                        }
-                    }
-
-                    //
-                    // If the value is negative, then place the minus sign
-                    // before the number.
-                    //
-                    if(ulNeg)
-                    {
-                        //
-                        // Place the minus sign in the output buffer.
-                        //
-                        pcBuf[ulPos++] = '-';
-                    }
-
-                    //
-                    // Convert the value into a string.
-                    //
-                    for(; ulIdx; ulIdx /= ulBase)
-                    {
-                        pcBuf[ulPos++] = g_pcHex[(ulValue / ulIdx) % ulBase];
-                    }
-
-                    //
-                    // Write the string.
-                    //
-                    UARTwrite(pcBuf, ulPos);
-
-                    //
-                    // This command has been handled.
-                    //
-                    break;
+									
+                    // Convert the value to ASCII.
+                    convert(ulValue, ulCount, pcHex, ulNeg, cFill, 16);
+										break;
                 }
 
-                //
                 // Handle the %% command.
-                //
                 case '%':
                 {
-                    //
                     // Simply write a single %.
-                    //
                     UARTwrite(pcString - 1, 1);
 
-                    //
                     // This command has been handled.
-                    //
                     break;
                 }
 
-                //
                 // Handle all other commands.
-                //
                 default:
                 {
-                    //
                     // Indicate an error.
-                    //
                     UARTwrite("ERROR", 5);
 
-                    //
                     // This command has been handled.
-                    //
                     break;
                 }
             }
