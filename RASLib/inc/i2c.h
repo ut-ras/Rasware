e3f7b8580f2fbc53bbd3568010aa735c387c15ea
Robby <geomasta21@aol.com>
2013-09-15 14:06:15 -0500

    switched tenses, began documentation on pwm, motor, servo, and i2c

--- RASLib/inc/i2c.h
+++ RASLib/inc/i2c.h
@@ -42,54 +42,95 @@ typedef struct I2C tI2C;
 // Number of available I2C modules
 #define I2C_COUNT 6
 
-// Function to initialize an I2C module on a pair of pins
-// The returned pointer can be used by the Send and Recieve functions
+/**
+ * Initializes an I2C module on a pair of pins
+ * @param sda [TODO: describe sda]
+ * @param scl [TODO: describe scl]
+ * @return Pointer to a I2C module used by other i2c functions
+ */
 tI2C *InitializeI2C(tPin sda, tPin scl);
-    
-// This function returns true if the 
-// previous transaction was successful
+
+/**
+ * Checks if the previous i2c transaction was successful
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @return true if the previous i2c transaction was successful
+ */
 tBoolean I2CSuccess(tI2C *i2c);
     
-// This function sends data to an I2C address.
-// A callback can be passed and will be called when 
-// all of the data in the passed array is sent.
+/**
+ * Sends data to an I2C address
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being sent
+ * @param data Pointer to array of byes being sent
+ * @param len Number of bytes in data array
+ * @param callback Function that will be called when the data has been sent
+ * @param cbdata [TODO: describe cbdata]
+ */
 void I2CBackgroundSend(tI2C *i2c, unsigned char addr, 
                                   const unsigned char *data, unsigned int len,
                                   tCallback callback, void *cbdata);
 
-// This function sends data to an I2C addresss
-// Takes a pointer to an array to send from
-// Returns true if successful
+/**
+ * Sends data to an I2C address
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being sent
+ * @param data Pointer to array of byes being sent
+ * @param len Number of bytes in data array
+ * @return true if successful
+ */
 tBoolean I2CSend(tI2C *i2c, unsigned char addr, 
                             const unsigned char *data, unsigned int len);
 
-// This function receives data from an I2C address.
-// A callback can be passed and will be called when 
-// all of the data is loaded into the passed array.
+/**
+ * Receives data from an I2C address
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being received
+ * @param data Pointer to array of byes to store the data received
+ * @param len Number of bytes allocated in the data array
+ * @param callback Function that will be called when all of the data is received
+ * @param cbdata [TODO: describe cbdata]
+ */
 void I2CBackgroundReceive(tI2C *i2c, unsigned char addr, 
                                      unsigned char *data, unsigned int len,
                                      tCallback callback, void *cbdata);
     
-// This function sends data to an I2C addresss
-// Takes a pointer to an array to receive to
-// Returns true if successful
+/**
+ * Receives data from an I2C address
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being received
+ * @param data Pointer to array of byes to store the data received
+ * @param len Number of bytes allocated in the data array
+ * @return true if successful
+ */
 tBoolean I2CReceive(tI2C *i2c, unsigned char addr, 
                                unsigned char* data, unsigned int len);
                                
-// This function requests data from an I2C address.
-// A callback can be passed and will be called when 
-// all of the data is loaded into the passed array.
+/**
+ * Requests data from an I2C address (nonblocking). This is the same as two sequential send and recieve calls but takes place in the internal state machine.
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being requested
+ * @param sendData Pointer to array of byes being sent
+ * @param sendLen Number of bytes allocated in the send data array
+ * @param recData Pointer to array of byes to store the data received
+ * @param recLen Number of bytes allocated in the receive data array
+ * @param callback Function that will be called when all of the data is received
+ * @param cbdata [TODO: describe cbdata]
+ */
 void I2CBackgroundRequest(tI2C *i2c, unsigned char addr, 
                                      const unsigned char *sendData, unsigned int sendLen,
                                      unsigned char *recData, unsigned int recLen,
                                      tCallback callback, void *cbdata);
     
-// This function requests data from an I2C address.
-// Takes two pointers to arrays. The first is the data to send
-// and the second is to hold the data recieved. 
-// This is the same as two sequential send and recieve calls
-// but takes place in the internal state machine.
-// Returns true if successful
+/**
+ * Requests data from an I2C address (blocking). This is the same as two sequential send and recieve calls but takes place in the internal state machine.
+ * @param i2c An initialized I2C module, which is returned by InitializeI2C
+ * @param addr I2C address where data is being requested
+ * @param sendData Pointer to array of byes being sent
+ * @param sendLen Number of bytes allocated in the send data array
+ * @param recData Pointer to array of byes to store the data received
+ * @param recLen Number of bytes allocated in the receive data array
+ * @return true if successful
+ */
 tBoolean I2CRequest(tI2C *i2c, unsigned char addr, 
                                const unsigned char *sendData, unsigned int sendLen,
                                unsigned char *recData, unsigned int recLen);
