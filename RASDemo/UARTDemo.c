6afeb8387002f5ff052bf6181cb3f5e1c8905f62
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2013-09-24 14:03:16 -0500

    Updated UARTDemo.c file, loops better and removed brackets

--- RASDemo/UARTDemo.c
+++ RASDemo/UARTDemo.c
@@ -4,21 +4,14 @@
 #include <RASLib/inc/uart.h>
 
 void uartDemo(void) {
-	Printf("tell me something! (science)\n-> ");
-	
-	{
-		char charArray[100];
+	char charArray[100];
+	char ESC = 27;
+	char ch = 0;
+	while(ch != ESC) {
+		Printf("tell me something! (science)\n-> ");
 		Gets(charArray, 100);
 		Printf("you said, \"%s\"\n", charArray);
-		Printf("thanks for the input!\n");
-	}
-	
-	{
-		char newline = 13;
-		char ch = Getc();
-		while(ch != newline) {
-			ch = Getc();
-			Putc(ch);
-		}
+		Printf("Thanks for the input! Press ESC to exit, or any other key to continue\n");
+		ch = Getc();
 	}
 }
