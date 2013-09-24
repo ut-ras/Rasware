ce08da1a915adab30d60abd4ec820992abf841fd
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2013-09-24 12:03:53 -0500

    Updated servodemo comments

--- RASDemo/ServoDemo.c
+++ RASDemo/ServoDemo.c
@@ -10,56 +10,48 @@ void initServo(void) {
 }
 
 void servoDemo(void) {
-		ch = Getc();
 	/**************************************************
 	* Servo takes position argument 0.00f-1.00f
 	*
 	**************************************************/
 	Printf("Press:\n  a-'up' 0.10\n  w-'up' 0.01\n  s-'down' 0.01\n");
 	Printf("  d-'down' 0.10\n  enter-quit\n");
+	float position = 0;
+	char newline = 13;
+	char ch;
 	
-	{   float position = 0;
-		char newline = 13;
-		char ch;
-		
-		Printf("position: %f ", position);
-		ch = Getc();
+	Printf("position: %f ", position);
+	ch = Getc();
         
-		while(ch != newline) {
-			switch(ch) {
-				case 'w':
-					position += 0.01f;
-					break;
-				case 's':
-					position -= 0.01f;
-					break;
-				case 'a':
-					position += 0.10f;
-					break;
-				case 'd':
-					position -= 0.10f;
-					break;
-				default:
-					position = position;
-			}
-			// Update from 2012, includes bounds checking
-            if(position > 1.00f) {
-			 	position = 1.00f;
-			} else if(position < 0.00f) {
-			 	position = 0.00f;
-			}
-			SetServo(servo, position);
-            
-			Printf("\rposition: %f ",position);	 
-			ch = Getc();
+   	while(ch != newline) {
+		switch(ch) {
+			case 'w':
+				position += 0.01f;
+				break;
+			case 's':
+				position -= 0.01f;
+				break;
+			case 'a':
+				position += 0.10f;
+				break;
+			case 'd':
+				position -= 0.10f;
+				break;
+			default:
+				position = position;
 		}
-				 
-		Printf("\n");
-	}
-      Printf("\rposition: %f ",position);   
-      ch = Getc();
-    }
-         
-    Printf("\n");
-  }
+		// Update from 2012, includes bounds checking (done with SetServo,
+		//   but makes user function easier to decrease.
+        if(position > 1.00f) {
+		 	position = 1.00f;
+		} else if(position < 0.00f) {
+		 	position = 0.00f;
+		}
+
+		SetServo(servo, position);
+           
+		Printf("\rposition: %f ",position);	 
+		ch = Getc();
+	}		 
+	Printf("\n");
 }
