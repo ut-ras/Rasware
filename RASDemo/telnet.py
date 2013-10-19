cd3ce377c237813c2afa48e8a9bc7871491f88b4
Christopher Haster <chaster@utexas.edu>
2013-10-19 18:27:48 -0500

    Cleaned up rasware.
    Removed some unused files, added a few include paths

--- RASDemo/telnet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python 
-
-import telnetlib , sys
-
-connection = telnetlib.Telnet("localhost","4444")
-for command in sys.argv:
-	connection.read_until(b">")
-	connection.write(command.encode('ascii') + b"\n")
-connection.close()
