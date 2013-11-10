d538334bc1589336154b9279c36d68707c0c2b18
Robert Nevels <geomasta21@aol.com>
2013-11-09 22:30:58 -0600

    added simple pid controller

--- NavLib/inc/pid.h
+++ NavLib/inc/pid.h
@@ -3,11 +3,13 @@
 
 typedef struct {
     float p, i d;
+    float maxOutput, minOutput;
     float prevErr, accumErr;
 } tPID;
 
 float RunPID(tPID *pid, float desired, float current);
 
-InitializePID(tPID* pid, float p, float i, float d);
+InitializePID(tPID* pid, float p, float i, float d, float min, float max) {
 
 #endif // __PID_H__
+
