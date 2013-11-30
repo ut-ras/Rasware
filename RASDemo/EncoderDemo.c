10048ac32c93c4e28100a87a61bdd682bd889de4
robz <rjnevels@utexas.edu>
2013-11-29 19:50:50 -0600

    corrected print statement, changed other spacing nitpicks, renamed to left and right encoders

--- RASDemo/EncoderDemo.c
+++ RASDemo/EncoderDemo.c
@@ -3,25 +3,24 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/encoder.h>
 
-
-tEncoder *demoEncoder[2];
+tEncoder *leftEncoder;
+tEncoder *rightEncoder;
 
 void initEncoders(void) {
-    demoEncoder[0] = InitializeEncoder(PIN_D0, PIN_D1, false);
-    demoEncoder[1] = InitializeEncoder(PIN_D2, PIN_D3, false);
+    leftEncoder = InitializeEncoder(PIN_D0, PIN_D1, false);
+    rightEncoder = InitializeEncoder(PIN_D2, PIN_D3, false);
 }
 
 void encoderDemo(void) {
-    Printf("Press:\nany key-read encoder values\n");
-    Printf("any key after read begins-quit\n");
+    Printf("Press:\n  any key after read begins-quit\n");
     
-    ResetEncoder(demoEncoder[0]);
-    ResetEncoder(demoEncoder[1]);
+    ResetEncoder(leftEncoder);
+    ResetEncoder(rightEncoder);
 
-    while(!KeyWasPressed()) {
+    while (!KeyWasPressed()) {
         Printf("enc0:%d  enc1:%d\r",
-                    GetEncoder(demoEncoder[0]),
-                    GetEncoder(demoEncoder[1]));
+            GetEncoder(leftEncoder),
+            GetEncoder(rightEncoder));
     }
     
     Printf("\n");
