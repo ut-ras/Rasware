enso622/Rasware2013

formated motor demo to remove extra closing brackets and do other nitpicks

RASDemo/MotorDemo.c
@@ -3,75 +3,91 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/motor.h>
 
-tMotor *motors[2];
+tMotor* leftMotor;
+tMotor* rightMotor;
 
 void initMotors(void) {
-    motors[0] = InitializeMotor(PIN_B7, PIN_B6, true, false);
-    motors[1] = InitializeMotor(PIN_C5, PIN_C4, true, false);
-    //motors[0] = InitializeMotor(PIN_F1, PIN_F0, true, false);
-    //motors[1] = InitializeMotor(PIN_F3, PIN_F2, true, false);
+    leftMotor = InitializeMotor(PIN_B7, PIN_B6, true, false);
+    rightMotor = InitializeMotor(PIN_C5, PIN_C4, true, false);
+    //leftMotor = InitializeMotor(PIN_F1, PIN_F0, true, false);
+    //rightMotor = InitializeMotor(PIN_F3, PIN_F2, true, false);
 
-    SetMotor(motors[0], 0);
-    SetMotor(motors[1], 0);
+    SetMotor(leftMotor, 0);
+    SetMotor(rightMotor, 0);
 }
 
 void motorDemo(void) {
-    Printf("Press:\n  w-forward\n  s-backward\n  a-left\n  ");
-    Printf("d-right\n  space-stop\n  enter-quit\n");
-  
-    {
-        float left = 0, right = 0, speed = 0.75f, accel = 0.01f;
-        char newline = 13;
-        char ch = Getc();
-        while(ch != newline) {
-            ch = Getc();
-            Printf("%c", ch);
-            switch(ch) {
-                case 'w':
-                    left = speed;
-                    right = speed;
-                    break;
-                case 's':
-                    left = -speed;
-                    right = -speed;
-                    break;
-                case 'a':
-                    left = -speed;
-                    right = speed;
-                    break;
-                case 'd':
-                    left = speed;
-                    right = -speed;
-                    break;
-                case 'i':
-                    right += accel;
-                    left += accel;
-                    break;
-                case 'k':
-                    right -= accel;
-                    left -= accel;
-                    break;
-                case 'j':
-                    right -= accel;
-                    left += accel;
-                    break;
-                case 'l':
-                    right += accel;
-                    left -= accel;
-                    break;
-                default:
-                    left = 0; 
-                    right = 0;
-                    break;
-            }
+    float left = 0, right = 0, speed = 0.75f, accel = 0.01f;
+    char newline = 13;
+    char ch;    
 
-            SetMotor(motors[0], left);
-            SetMotor(motors[1], right);
-            Printf(" Set Motor to %d %d  \r", (int)(left*100), (int)(right*100));
+    Printf("Press:\n  w-forward\n  s-backward\n  a-left\n  d-right\n");  
+    Printf("  i-slowly forward\n  k-backward\n  j-left\n  l-right\n");  
+    Printf("  space-stop\n  enter-quit\n");
+  
+    ch = Getc();
+    
+    while (ch != newline) {
+        switch (ch) {
+            case 'w':
+                left = speed;
+                right = speed;
+                break;
+            case 's':
+                left = -speed;
+                right = -speed;
+                break;
+            case 'a':
+                left = -speed;
+                right = speed;
+                break;
+            case 'd':
+                left = speed;
+                right = -speed;
+                break;
+            case 'i':
+                right += accel;
+                left += accel;
+                break;
+            case 'k':
+                right -= accel;
+                left -= accel;
+                break;
+            case 'j':
+                right -= accel;
+                left += accel;
+                break;
+            case 'l':
+                right += accel;
+                left -= accel;
+                break;
+            default:
+                left = 0; 
+                right = 0;
+                break;
         }
+        
+        // bounds checking (done in SetMotor, but also useful to bound it for the demo)
+        if (left > 1.0f) {
+             left = 1.0f;
+        } else if (left < -1.0f) {
+             left = -1.0f;
+        }
+
+        if (right > 1.0f) {
+             right = 1.0f;
+        } else if (right < -1.0f) {
+             right = -1.0f;
+        }
+
+        SetMotor(leftMotor, left);
+        SetMotor(rightMotor, right);
+        Printf("%c  set motor to %1.2f %1.2f\r", ch, left, right);
+        
+        ch = Getc();
     }                 
     
-    SetMotor(motors[0], 0.0f);
-    SetMotor(motors[1], 0.0f);
+    SetMotor(leftMotor, 0.0f);
+    SetMotor(rightMotor, 0.0f);
     Printf("\n");
 }

ut-ras/Rasware

RASDemo/MotorDemo.c
@@ -3,75 +3,91 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/motor.h>
 
-tMotor *motors[2];
+tMotor* leftMotor;
+tMotor* rightMotor;
 
 void initMotors(void) {
-    motors[0] = InitializeTLEMotor(PIN_B7, PIN_B6, true, false);
-    motors[1] = InitializeTLEMotor(PIN_C5, PIN_C4, true, false);
-    //motors[0] = InitializeTLEMotor(PIN_F1, PIN_F0, true, false);
-    //motors[1] = InitializeTLEMotor(PIN_F3, PIN_F2, true, false);
+    leftMotor = InitializeTLEMotor(PIN_B7, PIN_B6, true, false);
+    rightMotor = InitializeTLEMotor(PIN_C5, PIN_C4, true, false);
+    //leftMotor = InitializeTLEMotor(PIN_F1, PIN_F0, true, false);
+    //rightMotor = InitializeTLEMotor(PIN_F3, PIN_F2, true, false);
 
-    SetMotor(motors[0], 0);
-    SetMotor(motors[1], 0);
+    SetMotor(leftMotor, 0);
+    SetMotor(rightMotor, 0);
 }
 
 void motorDemo(void) {
-    Printf("Press:\n  w-forward\n  s-backward\n  a-left\n  ");
-    Printf("d-right\n  space-stop\n  enter-quit\n");
-  
-    {
-        float left = 0, right = 0, speed = 0.75f, accel = 0.01f;
-        char newline = 13;
-        char ch = Getc();
-        while(ch != newline) {
-            ch = Getc();
-            Printf("%c", ch);
-            switch(ch) {
-                case 'w':
-                    left = speed;
-                    right = speed;
-                    break;
-                case 's':
-                    left = -speed;
-                    right = -speed;
-                    break;
-                case 'a':
-                    left = -speed;
-                    right = speed;
-                    break;
-                case 'd':
-                    left = speed;
-                    right = -speed;
-                    break;
-                case 'i':
-                    right += accel;
-                    left += accel;
-                    break;
-                case 'k':
-                    right -= accel;
-                    left -= accel;
-                    break;
-                case 'j':
-                    right -= accel;
-                    left += accel;
-                    break;
-                case 'l':
-                    right += accel;
-                    left -= accel;
-                    break;
-                default:
-                    left = 0; 
-                    right = 0;
-                    break;
-            }
+    float left = 0, right = 0, speed = 0.75f, accel = 0.01f;
+    char newline = 13;
+    char ch;    
 
-            SetMotor(motors[0], left);
-            SetMotor(motors[1], right);
-            Printf(" Set Motor to %d %d  \r", (int)(left*100), (int)(right*100));
+    Printf("Press:\n  w-forward\n  s-backward\n  a-left\n  d-right\n");  
+    Printf("  i-slowly forward\n  k-backward\n  j-left\n  l-right\n");  
+    Printf("  space-stop\n  enter-quit\n");
+  
+    ch = Getc();
+    
+    while (ch != newline) {
+        switch (ch) {
+            case 'w':
+                left = speed;
+                right = speed;
+                break;
+            case 's':
+                left = -speed;
+                right = -speed;
+                break;
+            case 'a':
+                left = -speed;
+                right = speed;
+                break;
+            case 'd':
+                left = speed;
+                right = -speed;
+                break;
+            case 'i':
+                right += accel;
+                left += accel;
+                break;
+            case 'k':
+                right -= accel;
+                left -= accel;
+                break;
+            case 'j':
+                right -= accel;
+                left += accel;
+                break;
+            case 'l':
+                right += accel;
+                left -= accel;
+                break;
+            default:
+                left = 0; 
+                right = 0;
+                break;
         }
+        
+        // bounds checking (done in SetMotor, but also useful to bound it for the demo)
+        if (left > 1.0f) {
+             left = 1.0f;
+        } else if (left < -1.0f) {
+             left = -1.0f;
+        }
+
+        if (right > 1.0f) {
+             right = 1.0f;
+        } else if (right < -1.0f) {
+             right = -1.0f;
+        }
+
+        SetMotor(leftMotor, left);
+        SetMotor(rightMotor, right);
+        Printf("%c  set motor to %1.2f %1.2f\r", ch, left, right);
+        
+        ch = Getc();
     }                 
     
-    SetMotor(motors[0], 0.0f);
-    SetMotor(motors[1], 0.0f);
+    SetMotor(leftMotor, 0.0f);
+    SetMotor(rightMotor, 0.0f);
     Printf("\n");
 }